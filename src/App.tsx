080544521f1579f73bc07f771eadf1f7aa6ff16b
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2023-02-11 17:36:55 -0600

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Joke {
+  joke: string;
+}
 
 function App() {
-  return (
-    <div>
-      <header>
-        Sanity check
-      </header>
-    </div>
-  );
+  const [chuck, setChuck] = useState<Joke | null>(null);
+
+  useEffect(() => {
+    chuckFetch();
+  }, []);
+
+  const chuckFetch = async () => {
+    const response = await axios.get<Joke>(
+      "https://api.api-ninjas.com/v1/chucknorris?",
+      { headers: { "X-Api-Key": "lZOkNRcxcRbTIitJ2BjLKw==7ukbxScp64NxhMke" } }
+    );
+    setChuck(response.data);
+    console.log(chuck);
+  };
+
+  return <div>{chuck?.joke}</div>;
 }
 
 export default App;
